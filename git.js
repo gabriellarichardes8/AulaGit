08251bc59c256699dd1fbcd3afd2c0001e02c069
Gabriella <gabrielarichardes@gmail.com>
2025-04-07 15:43:21 -0300

    Testes no código

--- git.js
+++ git.js
@@ -3,3 +3,4 @@ console.log(`Lista de todas as comidas: ${comidas}`);
 console.log(`Primeira comida: ${comidas[0]}`);
 comidas.sort();
 console.log(`Comidas ordenadas: ${comidas}`);
+console.log('FIM');
